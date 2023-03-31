34578c784d5acb8ca0a8f0dd4f0e1eb64bb86b4d
Mohichehra <xudoyberdiyevam25@gmail.com>
2023-04-01 00:38:51 +0500

    inputsC

---
+++ src/components/Inputs2S/style.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+
+export const InputsTitle = styled.h2`
+  font-weight: 500;
+  font-size: 26px;
+  line-height: 16px;
+  margin-bottom: 20px;
+`;
+
+export const InputsText = styled.p`
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 16px;
+  color: var(--color);
+  margin-bottom: 40px;
+`;
+
+export const InputsList = styled.ul`
+  list-style-type: circle;
+  margin-bottom: 50px;
+`;
+export const LayoutWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  // align-items: flex-start;
+  gap: 25px;
+`;
+
+export const LayoutContainer = styled.div`
+  display: flex;
+  // align-items: flex-start;
+  flex-wrap: wrap;
+  // width: 50%;
+  gap: 25px;
+`;
+// export { Container, CardContainer };
